b8c51d2cad1e3120f6c39e3033a8d06e4b0e6574
AbdullahDsouky220 <abdallahdsoukyabdelnabyhassen30_sd@science.bsu.edu.eg>
2023-07-31 01:13:37 +0300

    adding some fetures

---
+++ pages/watch/[movieId.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import useMovie from '../../hooks/useMovie';
+
+const Watch = () => {
+  const router = useRouter();
+  const { movieId } = router.query;
+
+  const { data } = useMovie(movieId );
+  
+  return (
+    <>
+    <div className="h-screen w-screen bg-black">
+    <nav className="fixed w-full p-4 z-10 flex flex-row items-center gap-8 bg-black bg-opacity-70">
+    <div   
+onClick={() => router.push('/')}
+className="w-4 md:w-10 text-white cursor-pointer hover:opacity-80 transition" >
+<svg 
+xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+<path strokeLinecap="round" strokeLinejoin="round" d="M11.25 9l-3 3m0 0l3 3m-3-3h7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>
+</div>
+<p className="text-white text-1xl md:text-3xl font-bold">
+          <span className="font-light">
+          Watching:</span> {data?.title}
+        </p>
+      </nav>
+      <video className="h-full w-full" autoPlay controls src={data?.videoUrl}></video>
+      </div>
+      </>
+      )
+}
+
+export default Watch;
