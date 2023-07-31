bf41b8655e8e7f525f03f77f5051fb1cf4165e48
AbdullahDsouky220 <abdallahdsoukyabdelnabyhassen30_sd@science.bsu.edu.eg>
2023-07-31 03:52:23 +0300

    addomg

--- pages/api/movies/index.ts
+++ pages/api/movies/index.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import prismadb from '../../../lib/fetcher'
+import prismadb from '../../../lib/prismadb'
 import serverAuth from "../../../lib/serverAuth";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
